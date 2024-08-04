8af1fc60a5b26efe86601d2794be3587ea696120
Haneul Jang <haneul110721@gmail.com>
2024-08-03 20:57:07 -0400

    first commit

---
+++ JavaScript/main.js
@@ -0,0 +1,41 @@
+const btn = document.getElementById('cal');
+const result = document.getElementById('result');
+const totalProfitDiv = document.getElementById('total_profit');
+const resultingAmountDiv = document.getElementById('resulting_amount')
+
+btn.addEventListener('click', function() {
+    const input1 = document.getElementById('Initial').value;
+    const input2 = document.getElementById('time').value;
+    const input3 = document.getElementById('rate').value;
+
+    const P = Number(input1);
+    const t = Number(input2);
+    const r = Number(input3) / 100;
+    const n = 12; // Set the compounding period to 12 times per year
+
+    let total = P;
+    let totalProfit = 0;
+    const tbody = document.querySelector('#table_results tbody');
+    tbody.innerHTML = ''; // Reset previous results
+
+    for (let i = 1; i <= t; i++) {
+        const profit = total * r;
+        total += profit;
+        totalProfit += profit;
+
+        // Add the results to the table
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${i}</td>
+            <td>+${profit.toFixed(2)}</td>
+            <td>${total.toFixed(2)}</td>
+            <td>${(r * 100).toFixed(2)}%</td>
+        `;
+        tbody.appendChild(row);
+    }
+
+    totalProfitDiv.innerHTML = `${totalProfit.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`;
+    resultingAmountDiv.innerText = `${total.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`;
+    // const A = P * Math.pow((1 + r / n), n * t);
+    result.value = isNaN(total) ? 'Invalid input' : total.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+});
